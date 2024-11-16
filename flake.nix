d41a43894055b2db8f0b1045aa1583d526885cfb
Otto G <5030059+ottodevs@users.noreply.github.com>
2024-11-17 02:07:18 +0700

    chore: improve nix config

--- flake.nix
+++ flake.nix
@@ -1,4 +1,18 @@
 {
+  nixConfig = {
+    extra-substituters = [
+      "https://cache.nixos.org"
+      "https://nix-community.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+    allow-import-from-derivation = true;
+    preferPrebuilt = true;
+    accept-flake-config = true;
+  };
+
   description = "Bill ETHBangkok Nix Flake";
 
   inputs = {
@@ -16,7 +30,15 @@
 
       forAllSystems = f:
         nixpkgs.lib.genAttrs allSystems
-          (system: f { pkgs = import nixpkgs { inherit system; }; });
+          (system: f {
+            pkgs = import nixpkgs {
+              inherit system;
+              config = {
+                allowUnfree = true;
+                preferPrebuilt = true;
+              };
+            };
+          });
     in
     {
       devShells = forAllSystems ({ pkgs }: {
@@ -31,6 +53,8 @@
             protobuf
             jq
           ];
+
+          doCheck = false;
 
           shellHook = ''
             echo "Bill ETHBangkok Development Environment"
